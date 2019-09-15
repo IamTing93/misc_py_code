5f56f35084f2c4035de6e185c0715857c773cfd0
IamTing93 <IamTing93@163.com>
2019-09-15 22:40:22 +0800

    add code

---
+++ dynamic import.py
@@ -0,0 +1,58 @@
+def walk_modules(path):
+    """Loads a module and all its submodules from the given module path and
+    returns them. If *any* module throws an exception while importing, that
+    exception is thrown back.
+
+    For example: walk_modules('scrapy.utils')
+    """
+
+    mods = []
+    mod = import_module(path)
+    mods.append(mod)
+    # 递归获取子模块,dfs
+    if hasattr(mod, '__path__'):
+        for _, subpath, ispkg in iter_modules(mod.__path__):
+            fullpath = path + '.' + subpath
+            if ispkg:
+                mods += walk_modules(fullpath)
+            else:
+                submod = import_module(fullpath)
+                mods.append(submod)
+    return mods
+
+def iter_spider_classes(module):
+    """Return an iterator over all spider classes defined in the given module
+    that can be instantiated (ie. which have name)
+    """
+    # this needs to be imported here until get rid of the spider manager
+    # singleton in scrapy.spider.spiders
+    from scrapy.spiders import Spider
+
+    for obj in six.itervalues(vars(module)):
+        if inspect.isclass(obj) and \
+           issubclass(obj, Spider) and \
+           obj.__module__ == module.__name__ and \
+           getattr(obj, 'name', None):
+            yield obj
+
+def load_object(path):
+    """Load an object given its absolute object path, and return it.
+
+    object can be a class, function, variable or an instance.
+    path ie: 'scrapy.downloadermiddlewares.redirect.RedirectMiddleware'
+    """
+
+    try:
+        dot = path.rindex('.')
+    except ValueError:
+        raise ValueError("Error loading object '%s': not a full path" % path)
+
+    module, name = path[:dot], path[dot+1:]
+    mod = import_module(module)
+
+    try:
+        obj = getattr(mod, name)
+    except AttributeError:
+        raise NameError("Module '%s' doesn't define any object named '%s'" % (module, name))
+
+    return obj
